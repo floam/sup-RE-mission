f24345c4e6b6b0659fbcadeee28a1670094a2c63
Kaspar Kallas <git@kasparkallas.com>
2026-06-08 16:30:54 +0300

    Update mdx-components.tsx
    noop

--- sdk/docs/src/mdx-components.tsx
+++ sdk/docs/src/mdx-components.tsx
@@ -2,7 +2,7 @@ import defaultMdxComponents from "fumadocs-ui/mdx"
 import type { MDXComponents } from "mdx/types"
 import { Mermaid } from "./components/mdx/mermaid"
 
-// use this function to get MDX components, you will need it for rendering MDX
+// Use this function to get MDX components, you will need it for rendering MDX
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
 	return {
 		...defaultMdxComponents,
